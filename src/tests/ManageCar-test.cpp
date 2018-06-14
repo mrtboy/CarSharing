fd6e9646d6bd47c3a5e1f3b855d7540b4a3c1a9f
STUDIUM\Damian <damian-ulbrich@gmx.net>
2018-06-14 18:09:25 +0200

    test.

--- src/tests/ManageCar-test.cpp
+++ src/tests/ManageCar-test.cpp
@@ -4,13 +4,26 @@
 #include "Car.h"
 #include "../lib/ManageCar.h"
 #include "../lib/Car.h"
+#include <boost/asio/basic_socket_iostream.hpp>
+
 
 BOOST_AUTO_TEST_CASE(check_constructor)
 {
-	Car car{ "Reza",3500,"Kiel",1,"VW",2018,"Diesel",5,5,"?","6","Diesel",true };
-	ManageCar manageCar;
-	manageCar.insert(car);
 	
-	// Seems to work.
-	BOOST_CHECK_EQUAL(manageCar.reserveCar(1), true);
+	Car car1{ "Reza",3500,"Kiel",1,"VW",2018,"Diesel",5,5,"?","6","Diesel",true };
+	Car car2{ "Reza",3500,"Kiel",1,"VW",2018,"Diesel",5,5,"?","6","Diesel",true };
+	ManageCar manageCar;
+	manageCar.insert(car1);
+	manageCar.insert(car2);
+
+	
+
 }
+
+
+
+
+	
+	
+
+	
